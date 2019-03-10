83e67df63c3dfedef29066bff6c1fcb49a1366eb
Dana Simmons <dana@simmons-audio.com>
2019-03-10 09:45:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(driver) {
+    drivers.push(driver)
+    return drivers
+}
+
+function destructivelyPrependDriver(driver) {
+    drivers.unshift(driver)
+    return drivers
+}
+
+function destructivelyRemoveLastDriver(){
+    drivers.pop()
+    return drivers
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift()
+    return drivers
+}
+
+function appendDriver(driver) {
+    return drivers.concat(driver)
+}
+
+function prependDriver(driver) {
+    return [driver].concat(drivers);
+}
+
+function removeLastDriver() {
+    return drivers.slice(0,-1);
+}
+
+function removeFirstDriver() {
+    return drivers.slice(1);
+}
